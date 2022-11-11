d513d95603e9d27d4330901cf46c4e1d78b255f6
jurgenstevens <jurgen.stevens@yahoo.com>
2022-11-11 15:45:29 -0600

    Trail schema and table created

---
+++ db/migrate/20221111210825_create_trail.rb
@@ -0,0 +1,10 @@
+class CreateTrail < ActiveRecord::Migration[7.0]
+  def change
+    create_table :trails do |t|
+      t.string :trail_name
+      t.string :location
+      t.boolean :completed
+      t.timestamps
+    end
+  end
+end
