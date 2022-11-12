40765a4b471b22b66076f57f7933ab15a671c777
jurgenstevens <jurgen.stevens@yahoo.com>
2022-11-12 00:47:45 -0600

    Seed the db with User and Trail data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.create([{ name: "Dzenita" }, { name: "Gari" }, { name: "Matt" }])
+
+Trail.create(trail_name: "Capitan Peak via Summit Trail", location: "NM" , completed: false, user_id: 1)
+
+Trail.create(trail_name: "Big Carp River Trail", location: "MI" , completed: false, user_id: 2)
+
+Trail.create(trail_name: "Old Cloudcroft Trail Loop", location: "NM" , completed: true, user_id: 1)
+
+Trail.create(trail_name: "Rocky Bluff Trail", location: "IL" , completed: false, user_id: 3)
