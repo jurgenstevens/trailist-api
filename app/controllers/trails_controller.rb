109950a28195f7a21c4fccc603ded8d3070cc898
jurgenstevens <jurgen.stevens@yahoo.com>
2022-11-12 19:56:25 -0600

    Refactor trails index ctrl to one line.

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,6 +1,12 @@
 class TrailsController < ApplicationController
     # INDEX route to get all trails (HA!)
     def index
-        render json: ActiveRecord::Base.connection.execute('SELECT * FROM trails;')
+        render json: Trail.all
+        # or
+        # render json: ActiveRecord::Base.connection.execute('SELECT * FROM trails;')
+        # OR
+        # query_string = "SELECT * FROM trails;"
+        # results = ActiveRecord::Base.connection.execute(query_string)
+        # render json: results
     end
 end
