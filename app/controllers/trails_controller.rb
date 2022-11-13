7eaa3fa050542913eace2306d7a0e1a8c96beefa
jurgenstevens <jurgen.stevens@yahoo.com>
2022-11-12 22:10:26 -0600

    Complete the show controller to get trails by params id.

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,12 +1,20 @@
 class TrailsController < ApplicationController
     # INDEX route to get all trails (HA!)
     def index
-        render json: Trail.all
+        # render json: Trail.all
         # or
         # render json: ActiveRecord::Base.connection.execute('SELECT * FROM trails;')
-        # OR
+        # or
         # query_string = "SELECT * FROM trails;"
         # results = ActiveRecord::Base.connection.execute(query_string)
         # render json: results
+        trails = Trail.all
+        render json: {status: 200, trails: trails}
+    end
+
+    # SHOW route to get trails by id
+    def show
+        trail = Trail.find(params[:id])
+        render json: {status: 200, trail: trail}
     end
 end
