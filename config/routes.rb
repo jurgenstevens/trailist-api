fc6c10c2052924ed4964b16de5a96a7b0e17cd04
jurgenstevens <jurgen.stevens@yahoo.com>
2022-11-17 23:50:43 -0600

    Set the routes for trail reviews in a loop within trails in routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :trails
+  resources :trails, only: [:index, :show, :create] do
+    resources :trail_reviews, only: [:index, :show]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
